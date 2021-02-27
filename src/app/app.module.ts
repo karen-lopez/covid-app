517f190fbbfb875d2887b2df6ce76ce91e791280
Karen Lopez <stefanny@utp.edu.co>
2021-02-27 14:36:38 -0500

    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PatientsComponent } from './patients/patients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PatientsComponent
   ],
   imports: [
     BrowserModule
